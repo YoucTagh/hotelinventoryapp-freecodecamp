482755fe13d33a11927e7847a25ace190688b864
YoucTagh <youc.learn.13@gmail.com>
2023-03-19 09:47:16 +0100

    ngOnChange detection

--- src/app/room-list/room-list.component.ts
+++ src/app/room-list/room-list.component.ts
@@ -1,15 +1,21 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { RoomList } from '../rooms/rooms';
 
 @Component({
   selector: 'hinv-room-list',
   templateUrl: './room-list.component.html',
   styleUrls: ['./room-list.component.scss'],
+  changeDetection : ChangeDetectionStrategy.OnPush 
 })
-export class RoomListComponent implements OnInit {
+export class RoomListComponent implements OnInit,OnChanges {
   @Input() roomList: RoomList[] = [];
   @Output() selectedRoom: EventEmitter<RoomList> = new EventEmitter<RoomList>();
   constructor() {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+    
+  }
 
   ngOnInit(): void {}
 
--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -51,4 +51,18 @@ export class RoomsComponent implements OnInit {
   selectRoom(room: RoomList) {
     this.selectedRoom = room;
   }
+
+  addRoom() {
+    const room: RoomList = {
+      roomType: 'Deluxe Room Trio',
+      amenities: 'Air Conditioner',
+      price: 1200,
+      photos:
+        'https://images.unsplash.com/photo-1593624859907-dfe61e03dbeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80',
+      checkinTime: new Date('13-Nov-2022'),
+      checkoutTime: new Date('16-Nov-2022'),
+      rating: 3.353,
+    };
+    this.roomList = [...this.roomList, room];
+  }
 }
