07cf3b9c080e70660f501d0fe6815c8b44601261
YoucTagh <youc.learn.13@gmail.com>
2023-04-01 19:13:38 +0200

    ViewChild injection in ng-template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild, ViewContainerRef } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'hinv-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'hotelinventoryapp-freecodecamp';
+
+  @ViewChild('header',{read:ViewContainerRef})
+  vcr!:ViewContainerRef;
+
+  ngAfterViewInit(): void {
+      const componentRef= this.vcr.createComponent(HeaderComponent);
+      componentRef.instance.title = 'Go man!'
+  }
 }
