4de25f684fdde12c9e41850696a60eb12a794247
YoucTagh <youc.learn.13@gmail.com>
2023-05-23 19:55:40 +0200

    add initial Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
+import { ContainerComponent } from './container/container.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { EmployeeComponent } from './employee/employee.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'employees',
+    component: EmployeeComponent,
+  },
+  {
+    path: 'rooms',
+    component: RoomsComponent,
+  },
+  // {
+  //   path: 'container',
+  //   component: ContainerComponent,
+  // },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
