99891df7b51daedc9e167a232c0697fed1150a8d
YoucTagh <youc.learn.13@gmail.com>
2023-06-06 20:09:34 +0200

    ActivatedRoutes and params

--- src/app/rooms-booking/rooms-booking.component.ts
+++ src/app/rooms-booking/rooms-booking.component.ts
@@ -1,17 +1,24 @@
+import { map, Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'hinv-rooms-booking',
   templateUrl: './rooms-booking.component.html',
-  styleUrls: ['./rooms-booking.component.scss']
+  styleUrls: ['./rooms-booking.component.scss'],
 })
 export class RoomsBookingComponent implements OnInit {
+  id: number = 0;
 
-  id!:number;
-
-  constructor() { }
+  id$: Observable<number> = this.router.params.pipe(
+    map(params => params['id'])
+  );
+  constructor(private router: ActivatedRoute) {}
 
   ngOnInit(): void {
+    // this.id = this.router.snapshot.params['id'];
+    // this.router.params.subscribe((params) => {
+    //   this.id = params['id'];
+    // });
   }
-
 }
