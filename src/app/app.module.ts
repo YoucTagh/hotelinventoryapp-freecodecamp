230a3384e78edb923ee01a639db67bb835b3085b
YoucTagh <youc.learn.13@gmail.com>
2023-06-11 20:16:16 +0200

    add room using template driver forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { RoomsBookingComponent } from './rooms-booking/rooms-booking.component';
@@ -52,6 +53,7 @@ function initFactory(initService: InitService) {
     MatSidenavModule,
     MatIconModule,
     MatListModule,
+    MatInputModule,
     FormsModule
   ],
   providers: [
--- src/app/rooms-add/rooms-add.component.ts
+++ src/app/rooms-add/rooms-add.component.ts
@@ -1,15 +1,35 @@
+import { RoomsService } from './../shared/services/rooms/rooms.service';
+import { RoomList } from './../rooms/rooms';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'hinv-rooms-add',
   templateUrl: './rooms-add.component.html',
-  styleUrls: ['./rooms-add.component.scss']
+  styleUrls: ['./rooms-add.component.scss'],
 })
 export class RoomsAddComponent implements OnInit {
+  room: RoomList = {
+    roomType: '',
+    amenities: '',
+    checkinTime: new Date(),
+    checkoutTime: new Date(),
+    photos: '',
+    price: 0,
+    rating: 0,
+  };
 
-  constructor() { }
+  successMessage: string = ''
 
-  ngOnInit(): void {
+  constructor(private roomService: RoomsService) {}
+
+  ngOnInit(): void {}
+
+  addRoom() {
+    this.roomService.addRoom(this.room).subscribe(data=>{
+      console.log(data);
+      
+      this.successMessage = 'Data Added'
+    })
   }
-
 }
