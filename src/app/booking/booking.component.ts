448d78c03241b5621312ed7742ca7e8c55987223
YoucTagh <youc.learn.13@gmail.com>
2023-07-02 20:41:32 +0200

    intro to ReactiveForms

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,15 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  FormControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'hinv-booking',
   templateUrl: './booking.component.html',
-  styleUrls: ['./booking.component.scss']
+  styleUrls: ['./booking.component.scss'],
 })
 export class BookingComponent implements OnInit {
+  bookingForm!: FormGroup;
 
-  constructor() { }
+  // guests!:FormGroup[];
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.bookingForm = this.fb.group({
+      roomId: this.fb.control('1'),
+      guestEmail: ['test@gmail.com'],
+      checkinDate: [new Date('10-Feb-2020')],
+      bookingStatus: [''],
+      bookingAmount: [''],
+      bookingDate: [''],
+      mobileNumber: [''],
+      guestName: [''],
+      address: this.fb.group({
+        addressLine1: [''],
+        addressLine2: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: [''],
+      }),
+      guests: [],
+      tnc: false,
+    });
   }
 
+
+  addPassport(){
+
+  }
+  deletePassport(){}
+
+  addBooking(){
+
+  }
+
+  addGuest(){
+
+  }
+
+  removeGuest(index:number){
+
+  }
 }
--- src/app/booking/booking.module.ts
+++ src/app/booking/booking.module.ts
@@ -3,15 +3,30 @@ import { CommonModule } from '@angular/common';
 
 import { BookingRoutingModule } from './booking-routing.module';
 import { BookingComponent } from './booking.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
-  declarations: [
-    BookingComponent
-  ],
+  declarations: [BookingComponent],
   imports: [
     CommonModule,
-    BookingRoutingModule
-  ]
+    BookingRoutingModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatExpansionModule,
+  ],
 })
-export class BookingModule { }
+export class BookingModule {}
